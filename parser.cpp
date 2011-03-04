efa01528449e1e1a88c262aeea34b39f9f7417c7
Arnór Barkarson <arnorb06@ru.is>
2011-03-04 22:22:06 +0000

    all void funcs added, but not implemented, to parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -4,6 +4,83 @@ Parser::Parser(){
 	
 }
 
+//vantar
+
 void Parser::parseProgram(){
+	
+}
+void Parser::parseDeclarations(bool subProgramHead){
+	
+}
+void Parser::parseSubprogramDeclaration(){
+	
+}
+void Parser::parseSubprogramDeclarations(){
+	
+}
+void Parser::parseSubprogramHead(){
+	
+}
+void Parser::parseArguments(){
+	
+}
+void Parser::parseParameterList(){
+	
+}
+void Parser::parseParameterListMore(){
+	
+}
+void Parser::parseIdentifierList(EntryList *eList){
+	
+}
+void Parser::parseIdentifierListMore(EntryList *eList){
+	
+}
+void Parser::parseIdentifierListAndType(bool subProgramHead){
+	
+}
+void Parser::parseType(){
+	
+}
+void Parser::parseStandardType(){
+	
+}
+void Parser::parseCompoundStatement(){
+	
+}
+void Parser::parseOptionalStatement(){
+	
+}
+void Parser::parseStatementList(){
+	
+}
+void Parser::parseStatementListMore(){
+	
+}
+void Parser::parseStatement(){
+	
+}
+void Parser::parseIfStatement(){
+	
+}
+void Parser::parseWhileStatement(){
+	
+}
+void Parser::parseIdOrProcedureStatement(SymbolTableEntry* prevEntry){
+	
+}
+
+//vantar
+
+void Parser::parseExpressionList(SymbolTableEntry* prevEntry){
+	
+}
+void Parser::parseExpressionListMore(EntryList* eList){
+	
+}
+
+//vantar
+
+void Parser::parseArrayReference(){
 	
 }
