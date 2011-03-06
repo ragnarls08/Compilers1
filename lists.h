285c2ef759b66984fab472da6e8a961510a45d8e
Ragnar <ragnarls08@ru.is>
2011-03-06 22:12:20 +0000

    add lists.h

---
+++ lists.h
@@ -0,0 +1,12 @@
+#include "token.h"
+
+
+TokenCode pProgramDefinition[6] = { tc_SEMICOL, tc_VAR, tc_FUNCTION, tc_PROCEDURE, tc_BEGIN, (TokenCode)'\0' };
+
+TokenCode pDeclarations[1] = {(TokenCode)'\0'};
+TokenCode pSubprogramDeclarations[1] = {(TokenCode)'\0'};
+TokenCode pCompoundStatement[1] = {(TokenCode)'\0'};
+
+
+
+
